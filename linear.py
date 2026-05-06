d72261add16e04220d2f8aef8844d85d3b33867a
fizanazrin8-prog <fizanazrin8@gmail.com>
2026-05-06 09:49:46 +0400

    Update linear.py

--- linear.py
+++ linear.py
@@ -5,11 +5,14 @@ from sklear.linear_model import LinearRegression
 df=pd.read_csv("student_scores.csv")
 X=df.iloc[:, :-1].values
 Y=df.iloc[:, -1].values
-X_train,X_test,Y_train,Y_test=train_test_slpit(X,Y test_size=0.2, random=42)
+X_train,X_test,Y_train,Y_test=train_test_slpit(X,Y test_size=0.2, random_state=42)
 model=LinearRgression()
 model.fit(X_train,Y_train)
 st.title("Exam score prediction model")
 st.write("Enter the number of hours you studied for the exam")
 hours=st.number_input("Hours studied",min_value=0.0, step=0.1)
 if st.button"Predict score"):
-
+    predicted_score=model.predict([[hours]])[0]
+st.success(f"predicted score: .2f")
+st.write("Sample Training Data")
+st.dataframe(df)
