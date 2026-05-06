cfb2ed43e77b1bafaee3c746adce8f07eb58a79e
fizanazrin8-prog <fizanazrin8@gmail.com>
2026-05-06 09:58:16 +0400

    Update linear.py

--- linear.py
+++ linear.py
@@ -12,7 +12,7 @@ st.title("Exam score prediction model")
 st.write("Enter the number of hours you studied for the exam")
 hours=st.number_input("Hours studied",min_value=0.0, step=0.1)
 if st.button"Predict score"):
-    predicted_score=model.predict([[hours]])[0]
-    st.success(f"predicted score: {predicted_score: .2f}")
+ predicted_score=model.predict([[hours]])[0]
+ st.success(f"predicted score: {predicted_score: .2f}")
 st.write("Sample Training Data")
 st.dataframe(df)
